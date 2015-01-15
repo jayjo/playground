8b1c5ba68ee4cc7cd4accf7d08b6cb3d2f06717a
Jeffrey Jorgensen <Jeffrey@Jeffreys-MacBook-Pro.local>
2015-01-14 17:33:02 -0800

    pushing again as changed dont seem to have made it. Updated things and got the main template for sections coded. Some additional things are needed.

---
+++ Mixpanel/Marketing Style Guide/inc/left_area.php
@@ -0,0 +1,12 @@
+
+<div class="left_area colors">
+	<div class="content">
+		<h1>
+			<span>02</span>Color
+		</h1>
+		<p>The Mixpanel color palette has expanded to include dynamic colors that complement each other and can be used in any combination as long as the integrity of the brand remains intact.</p>
+	</div>
+	<footer>
+		<p><span class="page">03</span> Mixpanel Marketing & Brand Styleguide</p>
+	</footer>
+</div>
--- Mixpanel/Marketing Style Guide/index.php
+++ Mixpanel/Marketing Style Guide/index.php
@@ -1,5 +1,9 @@
 <?php include('inc/header.php'); ?>
 
+<!-- SECTION LEFT AREA -->
+  <?php include('inc/left_area.php'); ?>
+<!-- END SECTION LEFT AREA -->
+
 <!-- SECTION LOGO -->
 
 <!-- END SECTION LOGO -->
