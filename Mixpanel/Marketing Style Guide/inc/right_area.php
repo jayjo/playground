37e717c94dd49ed6d46d79407551f903a2f14996
Jeffrey Jorgensen <Jeffrey@Jeffreys-MacBook-Pro.local>
2015-01-15 16:01:57 -0800

    finished up the gradients area of the color area

--- Mixpanel/Marketing Style Guide/inc/right_area.php
+++ Mixpanel/Marketing Style Guide/inc/right_area.php
@@ -117,122 +117,68 @@
 		    </figure>
 		  </div>
 		  <!-- END SECONDARY -->
-		</section>
-		<!-- END COLORS -->
-		<!-- START COLORS -->
-		<section id="colors">
-			<!-- START PRIMARY -->
-			<h3 class="slate">Primary Colors</h3>
-			<p class="gray">These are first colors to be considered for a design. These primary colors reflect what should be used for text, links and lighter backgrounds.</p>
-		  <div class="section primary">
-		    <figure class="primary_color slate">
-		      <div class="color"></div>
-		      <h6 class="slate name">Slate</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="454E5A">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="69 78 90">
-		      </p>
-		    </figure>
-		    <figure class="primary_color blue">
-		      <div class="color"></div>
-		      <h6 class="slate name">Blue</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="45A0F8">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="69 160 248">
-		      </p>
-		    </figure>
-		    <figure class="primary_color gray">
-		      <div class="color"></div>
-		      <h6 class="slate name">Gray</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="8D989E">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="141 152 158">
-		      </p>
-		    </figure>
-		    <figure class="primary_color light_gray">
-		      <div class="color"></div>
-		      <h6 class="slate name">Light Gray</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="F7FAFC">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="247 250 252">
-		      </p>
-		    </figure>
-		  </div>
-		  <!-- END PRIMARY -->
-		  <!-- START SECONDARY -->
-			<h3 class="slate">Secondary Colors</h3>
-			<p class="gray">We have defined a core set of secondary colors for you to start with. These are to be used in the supporting illustrations throughout the site and in combination with the primary neutral colors.</p>
-		  <div class="section secondary">
-		    <figure class="secondary_color purple">
+		  <!-- START GRADIENTS -->
+			<h3 class="slate">Gradients</h3>
+			<p class="gray">Each feature on the marketing site will have it’s own unique gradient used in the hero background. These will otherwise be used minimally.</p>
+		  <div class="section gradient">
+		    <figure class="gradient_color purple_grad">
 		      <div class="color"></div>
 		      <h6 class="slate name">Purple</h6>
 		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="454E5A">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="69 78 90">
-		      </p>
-		    </figure>
-		    <figure class="secondary_color medium_blue">
-		      <div class="color"></div>
-		      <h6 class="slate name">Medium Blue</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="45A0F8">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="69 160 248">
+		        <input type="text" readonly="readonly" name="hex" value="454E5A" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
 		      </p>
 		    </figure>
-		    <figure class="secondary_color sage_green">
+		    <figure class="gradient_color deep_blue_grad">
 		      <div class="color"></div>
-		      <h6 class="slate name">Sage Green</h6>
+		      <h6 class="slate name">Deep Blue</h6>
 		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="8D989E">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="141 152 158">
+		        <input type="text" readonly="readonly" name="hex" value="45A0F8" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
 		      </p>
 		    </figure>
-		    <figure class="secondary_color red">
+		    <figure class="gradient_color green_grad">
 		      <div class="color"></div>
-		      <h6 class="slate name">Red</h6>
+		      <h6 class="slate name">Green</h6>
 		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="F7FAFC">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="247 250 252">
-		      </p>
-		    </figure>
-		    <figure class="secondary_color orange">
-		      <div class="color"></div>
-		      <h6 class="slate name">Orange</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="F7FAFC">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="247 250 252">
-		      </p>
-		    </figure>
-		    <figure class="secondary_color yellow">
-		      <div class="color"></div>
-		      <h6 class="slate name">Yellow</h6>
-		      <p class="desc hex">
-		        <input type="text" readonly="readonly" name="hex" value="F7FAFC">
-		      </p>
-		      <p class="desc rgb">
-		        <input type="text" readonly="readonly" name="rgb" value="247 250 252">
+		        <input type="text" readonly="readonly" name="hex" value="8D989E" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
 		      </p>
 		    </figure>
 		  </div>
-		  <!-- END SECONDARY -->
+		  <div class="section gradient">
+		    <figure class="gradient_color mars_grad">
+		      <div class="color"></div>
+		      <h6 class="slate name">Mars</h6>
+		      <p class="desc hex">
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		      </p>
+		    </figure>
+		    <figure class="gradient_color orange_grad">
+		      <div class="color"></div>
+		      <h6 class="slate name">Orange</h6>
+		      <p class="desc hex">
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		      </p>
+		    </figure>
+		    <figure class="gradient_color cold_blue_grad">
+		      <div class="color"></div>
+		      <h6 class="slate name">Cold Blue</h6>
+		      <p class="desc hex">
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		        <span>to</span>
+		        <input type="text" readonly="readonly" name="hex" value="F7FAFC" class="gradinput">
+		      </p>
+		    </figure>
+		  </div>
+		  <!-- END GRADIENTS -->
 		</section>
 		<!-- END COLORS -->
 		<!-- <?php include('inc/right.php'); ?> -->
