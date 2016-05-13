798c3b1552286f6c6727688c067f64abd992c657
Jeffrey Jorgensen <Jeffrey@Jeffreys-MacBook-Pro.local>
2016-05-13 16:56:47 -0700

    things

--- Mixpanel/CE Style Guide/assets/js/global.js
+++ Mixpanel/CE Style Guide/assets/js/global.js
@@ -1,12 +1,18 @@
 jQuery(document).ready(function($) {
 
-	$('.amount p').on('click', function(){
-		$(this).siblings('.answer').addClass('active');
-	});
+	hljs.initHighlightingOnLoad();
 
-	$('.answer span').on('click', function(){
-		$(this).parent().removeClass('active');
-		$(this).parent().siblings('p').hide();
-	});
+	function scrollyBiz(){
+		$(window).on('scroll', function(e){
+			var Y = $(window).scrollTop();
+			if( Y >= $(window).height() - 50 ) {
+				console.log($(this).scrollTop());
+				$('header').addClass('scrolled');
+			} else {
+				$('header').removeClass('scrolled');
+			}
+		});
+	}
+	scrollyBiz();
 
 });
