14345ae9869860efb796d7368becd5f34a544f35
Jeffrey Jorgensen <jeff@jayjodesign.com>
2016-09-20 07:09:44 -0700

    updated tables and added in some colors

---
+++ Personal/Top Draw Tables/index.php
@@ -0,0 +1,222 @@
+<?php
+
+$body_class = 'home';
+$page_title = 'home';
+
+$jquery_plugins = array(
+	'something.js'
+	);
+
+include 'inc/header.php';
+
+?>
+
+<section id="content">
+
+<table class="table tablesorter unittable" data-striped="true">
+	<thead>
+	<tr>
+		<th data-switchable="false">All Non-Resident Units/Hunts</th>
+		<th>Hunt Dates</th>
+		<th>2014 Draw Odds</th>
+		<th data-align="right" data-sortable="true">Success Rate(All Weapons)</th>
+		<th>Success Rate(Archery)</th>
+		<th>Avg. Score(All Weapons, Estimated)</th>
+		<th>Avg. Horn Length(All Weapons)</th>
+		<th>Avg. Horn Base(All Weapons)</th>
+		<th>Best Goat(All Weapons, Estimated)</th>
+		<th>Best Archery Goat(Estimated)</th>
+		<th>Avg. Age</th>
+		<th>Days Hunted</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>G02- Mount Princeton- Hunt 1</td>
+		<td>Sept. 2-Oct. 5</td>
+		<td class="drawOdds">367</td>
+		<td>76%</td>
+		<td>2.7%</td>
+		<td>42 1/8</td>
+		<td>8 2/8</td>
+		<td>4 5/8</td>
+		<td>51 7/8</td>
+		<td>37 3/8</td>
+		<td>5.7</td>
+		<td>5.4</td>
+	</tr>
+	<tr>
+		<td>G04- Mount Evans- Hunt 1</td>
+		<td>Sept. 15-26, Weekdays Only</td>
+		<td class="drawOdds">641</td>
+		<td>81.4493%</td>
+		<td>8.3%</td>
+		<td>39 7/8</td>
+		<td>7 4/8</td>
+		<td>4 4/8</td>
+		<td>49 1/8</td>
+		<td>44 5/8</td>
+		<td>5.0</td>
+		<td>3.1</td>
+	</tr>
+	<tr>
+		<td>G04- Mount Evans- Hunt 2</td>
+		<td>Sept. 29-Oct. 10, Weekdays Only</td>
+		<td class="drawOdds">406</td>
+		<td>81%</td>
+		<td>8.32392%</td>
+		<td>39 7/8</td>
+		<td>7 4/8</td>
+		<td>4 4/8</td>
+		<td>49 1/8</td>
+		<td>44 5/8</td>
+		<td>5.0</td>
+		<td>3.1</td>
+	</tr>
+	<tr>
+		<td>G04- Mount Evans- Hunt 3</td>
+		<td>Oct. 13-Oct. 24, Weekdays Only</td>
+		<td class="drawOdds">224</td>
+		<td>81%</td>
+		<td>8.34094309%</td>
+		<td>39 7/8</td>
+		<td>7 4/8</td>
+		<td>4 4/8</td>
+		<td>49 1/8</td>
+		<td>44 5/8</td>
+		<td>5.0</td>
+		<td>3.1</td>
+	</tr>
+	<tr>
+		<td>G05- West Needles- Hunt 1- Archery</td>
+		<td>Sept. 2-Oct. 31</td>
+		<td class="drawOdds">318</td>
+		<td>80%</td>
+		<td>80.0%</td>
+		<td>41</td>
+		<td>8 2/8</td>
+		<td>4 3/8</td>
+		<td>50 7/8</td>
+		<td>50 7/8</td>
+		<td>5.6</td>
+		<td>5.4</td>
+	</tr>
+	<tr>
+		<td>G07- Grays Peak- Hunt 2</td>
+		<td>Sept. 15-26, Weekdays Only</td>
+		<td class="drawOdds">225</td>
+		<td>85%</td>
+		<td>3.7%</td>
+		<td>41 4/8</td>
+		<td>8</td>
+		<td>4 5/8</td>
+		<td>48 2/8</td>
+		<td>46 2/8</td>
+		<td>4.7</td>
+		<td>2.5</td>
+	</tr>
+	<tr>
+		<td>G07- Grays Peak- Hunt 3</td>
+		<td>Sept. 29-Oct. 10, Weekdays Only</td>
+		<td class="drawOdds">212</td>
+		<td>85%</td>
+		<td>3.7%</td>
+		<td>41 4/8</td>
+		<td>8</td>
+		<td>4 5/8</td>
+		<td>48 2/8</td>
+		<td>46 2/8</td>
+		<td>4.7</td>
+		<td>2.5</td>
+	</tr>
+	<tr>
+		<td>G12- Maroon Bells- Hunt 1</td>
+		<td>Sept. 2-Oct. 5</td>
+		<td class="drawOdds">242</td>
+		<td>93%</td>
+		<td>2.2%</td>
+		<td>41 3/8</td>
+		<td>7 7/8</td>
+		<td>4 5/8</td>
+		<td>49</td>
+		<td>42 5/8</td>
+		<td>6.1</td>
+		<td>2.2</td>
+	</tr>
+	<tr>
+		<td>G13- Quail Mountain- Hunt 1</td>
+		<td>Sept. 2-18</td>
+		<td class="drawOdds">103</td>
+		<td>73%</td>
+		<td>0.0%</td>
+		<td>40 6/8</td>
+		<td>7 5/8</td>
+		<td>4 5/8</td>
+		<td>47 4/8</td>
+		<td>0</td>
+		<td>5.2</td>
+		<td>3.5</td>
+	</tr>
+	<tr>
+		<td>G13- Quail Mountain-Hunt 2</td>
+		<td>Sept. 19-Oct. 5</td>
+		<td class="drawOdds">87</td>
+		<td>73%</td>
+		<td>0.0%</td>
+		<td>40 6/8</td>
+		<td>7 5/8</td>
+		<td>4 5/8</td>
+		<td>47 4/8</td>
+		<td>0</td>
+		<td>5.2</td>
+		<td>3.5</td>
+	</tr>
+	<tr>
+		<td>G15- Jones Pass- Hunt 2</td>
+		<td>Sept. 15-26, Weekdays Only</td>
+		<td class="drawOdds">124</td>
+		<td>68%</td>
+		<td>4.0%</td>
+		<td>40 6/8</td>
+		<td>8</td>
+		<td>4 4/8</td>
+		<td>51 3/8</td>
+		<td>48 4/8</td>
+		<td>4.9</td>
+		<td>3.4</td>
+	</tr>
+	<tr>
+		<td>G16- Mount Guyot- Hunt 1</td>
+		<td>Sept. 2-12, Weekdays Only</td>
+		<td class="drawOdds">306</td>
+		<td>87%</td>
+		<td>19.4%</td>
+		<td>39 2/8</td>
+		<td>7 6/8</td>
+		<td>4 2/8</td>
+		<td>50 5/8</td>
+		<td>43 3/8</td>
+		<td>4.3</td>
+		<td>2.7</td>
+	</tr>
+	<tr>
+		<td>G17- Independence Pass- Hunt 1</td>
+		<td>Sept. 2-Oct. 5, Weekdays Only</td>
+		<td class="drawOdds">21</td>
+		<td>81%</td>
+		<td>3.8%</td>
+		<td>41 5/8</td>
+		<td>7 7/8</td>
+		<td>4 6/8</td>
+		<td>49 2/8</td>
+		<td>43 6/8</td>
+		<td>5.3</td>
+		<td>4.9</td>
+	</tr>
+	</tbody>
+</table>
+
+<input type="text" value="" class="calc">
+</section>
+
+<?php include 'inc/footer.php'; ?>
